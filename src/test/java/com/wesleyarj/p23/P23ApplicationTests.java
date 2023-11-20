e7478398f63f22925d0049e67315aa53e5ac743e
Wesley Araujo <wesleyjk85@gmail.com>
2023-11-20 20:17:53 -0300

    Project criated

---
+++ src/test/java/com/wesleyarj/p23/P23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyarj.p23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
