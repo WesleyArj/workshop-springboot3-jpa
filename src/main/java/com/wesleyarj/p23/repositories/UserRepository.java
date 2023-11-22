995e2a6eef49ccd13867711003917cebbaa13f7e
Wesley Araujo <wesleyjk85@gmail.com>
2023-11-22 16:50:05 -0300

    Service layer, component registration

--- src/main/java/com/wesleyarj/p23/repositories/UserRepository.java
+++ src/main/java/com/wesleyarj/p23/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wesleyarj.p23.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/wesleyarj/p23/services/UserService.java
@@ -0,0 +1,28 @@
+package com.wesleyarj.p23.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesleyarj.p23.entities.User;
+import com.wesleyarj.p23.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	
+	public User findById (Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
