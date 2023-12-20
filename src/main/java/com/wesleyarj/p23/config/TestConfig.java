56017fb3a5547ec4192d5962220da958dfc0960a
Wesley Araujo <wesleyjk85@gmail.com>
2023-12-20 20:14:15 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/wesleyarj/p23/config/TestConfig.java
+++ src/main/java/com/wesleyarj/p23/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.wesleyarj.p23.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.wesleyarj.p23.entities.Category;
 import com.wesleyarj.p23.entities.Order;
+import com.wesleyarj.p23.entities.OrderItem;
 import com.wesleyarj.p23.entities.Product;
 import com.wesleyarj.p23.entities.User;
 import com.wesleyarj.p23.entities.enums.OrderStatus;
 import com.wesleyarj.p23.repositories.CategoryRepository;
+import com.wesleyarj.p23.repositories.OrderItenRepository;
 import com.wesleyarj.p23.repositories.OrderRepository;
 import com.wesleyarj.p23.repositories.ProductRepository;
 import com.wesleyarj.p23.repositories.UserRepository;
@@ -34,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository PrRp;
+	
+	@Autowired
+	private OrderItenRepository oir;
 	
 
 	@Override
@@ -70,6 +74,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		UsRp.saveAll(Arrays.asList(u1, u2));
 		OrRp.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		oir.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
---
+++ src/main/java/com/wesleyarj/p23/entities/OrderItem.java
@@ -0,0 +1,89 @@
+package com.wesleyarj.p23.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.wesleyarj.p23.entities.pk.OrderItenPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_order_iten")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItenPK id = new OrderItenPK();
+	
+	private Integer quantiti;
+	private Double price;
+	
+	public OrderItem(){}
+
+	public OrderItem(Order order, Product product, Integer quantiti, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantiti = quantiti;
+		this.price = price;
+	}
+	
+	@JsonIgnore
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+
+	public Integer getQuantiti() {
+		return quantiti;
+	}
+
+	public void setQuantiti(Integer quantiti) {
+		this.quantiti = quantiti;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+
+}
